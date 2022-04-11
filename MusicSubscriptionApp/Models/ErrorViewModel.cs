bb4bc24076953058998c59dfe65d47af030217f9
JoshP85 <joshua.phillips1985@gmail.com>
2022-04-12 09:14:53 +1000

    Add project files.

---
+++ MusicSubscriptionApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MusicSubscriptionApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MusicSubscriptionApp/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
+
+builder.WebHost.UseUrls("http://*:5000");  
+
+var app = builder.Build();
+
+app.UseForwardedHeaders();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+    app.UseHttpsRedirection();
+}
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
